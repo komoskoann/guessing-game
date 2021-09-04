89ab6b05dc81e1acb7563109c87ecab9bd43251f
komoskoann\\----------- <komoskoann@gmail.com>
2021-09-04 19:18:41 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
     constructor() {}
+    
+    range = {
+        min: 0,
+        max: 0,
+    }    
 
     setRange(min, max) {
-
+        this.range.min = min;
+        this.range.max = max;
     }
 
     guess() {
-
+        return Math.round((this.range.max - this.range.min) / 2) + this.range.min;
     }
 
     lower() {
-
+        this.range.max = this.guess();
     }
 
     greater() {
-
+        this.range.min = this.guess();
     }
 }
 
